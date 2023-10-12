fc5837b25239ce1198d322a0fb89dd2b6724e423
lanceydancey <lanceomiller@yahoo.com>
2023-10-12 11:37:23 -0700

    trying to get library functions written

--- toy-rsa/src/lib.rs
+++ toy-rsa/src/lib.rs
@@ -1,4 +1,4 @@
-use toy_rsa_lib::*
+use toy_rsa_lib::*;
 
 /// Fixed RSA encryption exponent.
 pub const EXP: u64 = 65_537;
@@ -6,11 +6,12 @@ pub const EXP: u64 = 65_537;
 /// Generate a pair of primes in the range `2**31..2**32`
 /// suitable for RSA encryption with exponent.
 pub fn genkey() -> (u32, u32)
-
+    
 /// Encrypt the plaintext `msg` using the RSA public `key`
 /// and return the ciphertext.
 pub fn encrypt(key: u64, msg: u32) -> u64
-
+    msg.pow(EXP) % key
 /// Decrypt the cipertext `msg` using the RSA private `key`
 /// and return the resulting plaintext.
 pub fn decrypt(key: (u32, u32), msg: u64) -> u32
+    d = 1/(EXP mod ())

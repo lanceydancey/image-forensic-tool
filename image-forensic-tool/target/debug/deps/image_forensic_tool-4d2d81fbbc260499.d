4fa584b8de5ae8a3f189a5388b3ffcccc838ca4d
Lance Miller <lance6@pdx.edu>
2023-11-20 17:03:07 -0800

    removing parent folder

--- image-forensic-tool/target/debug/deps/image_forensic_tool-4d2d81fbbc260499.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lance\OneDrive\Rust\image-forensic-tool\target\debug\deps\image_forensic_tool-4d2d81fbbc260499.rmeta: src\main.rs
-
-C:\Users\lance\OneDrive\Rust\image-forensic-tool\target\debug\deps\image_forensic_tool-4d2d81fbbc260499.d: src\main.rs
-
-src\main.rs:

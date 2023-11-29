510c2a9f4a2c308b9504673fbaba2c9b3b935bb8
lanceydancey <98341264+lanceydancey@users.noreply.github.com>
2023-11-29 10:45:56 -0800

    rustdoc done for library

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -13,6 +13,13 @@ pub struct ImageData {
 }
 
 // https://github.com/kamadak/exif-rs/blob/master/examples/reading.rs
+/// Reads the metadata from an image file and creates ImageData struct objects
+/// 
+/// This function takes in a path for an image object, reads the metadata using Exif, and construst ImageData objects which consist of 
+/// `filename`, `date_created`, `gps_coordinates`
+/// 
+/// # Argument
+/// * `file_path` - A reference to a path to an image file
 fn read_exif_data<P: AsRef<Path>>(file_path: P) -> Result<ImageData, String> {
     let path: &Path = file_path.as_ref();
     let exif: Exif = open_file(path)?;
@@ -28,6 +35,15 @@ fn read_exif_data<P: AsRef<Path>>(file_path: P) -> Result<ImageData, String> {
     })
 }
 
+/// Opens a file and reads its EXIF data.
+///
+/// This function takes a path to an image file, attempts to open it, and then reads its EXIF data.
+/// It returns an Exif object if succesful, an error message otherwise.
+///
+/// # Arguments
+///
+/// * `file_path` - A path pointing to the image file.
+///
 fn open_file<P: AsRef<Path>>(file_path: P) -> Result<Exif, String> {
     let mut file: File =
         File::open(file_path).map_err(|e| format!("Failed to open file: {}", e))?;
@@ -41,6 +57,12 @@ fn open_file<P: AsRef<Path>>(file_path: P) -> Result<Exif, String> {
         .map_err(|e| format!("Failed to read EXIF data: {}", e))
 }
 
+/// Extracts original date and time from an image file
+///
+/// This function will extract the data in the DateTimeOriginal tag and return it as a string if present.
+///
+/// # Arguments
+/// * `exif` - A reference to an Exif object
 fn get_date_time(exif: &Exif) -> Option<String> {
     exif.get_field(Tag::DateTimeOriginal, In::PRIMARY)
         .and_then(|field: &exif::Field| match field.value {
@@ -51,6 +73,13 @@ fn get_date_time(exif: &Exif) -> Option<String> {
         })
 }
 
+/// Extracts latitude and latitudinal reference from an image file
+///
+/// This function will extract the data in the GPSLatitude and GPSLatitudeRef tags, if present.
+/// It then formats it into decimal format and returns it as a string.
+///
+/// # Arguments
+/// * `exif` - A reference to an Exif object
 fn get_latitude(exif: &Exif) -> Option<String> {
     let latitude: Option<Vec<exif::Rational>> = exif
         .get_field(Tag::GPSLatitude, In::PRIMARY)
@@ -73,6 +102,13 @@ fn get_latitude(exif: &Exif) -> Option<String> {
     }
 }
 
+/// Extracts longitude and longitudinal reference from an image file
+///
+/// This function will extract the data in the GPSLongitude and GPSLongitudeRef tags, if present.
+/// It then formats it into decimal format and returns it as a string.
+///
+/// # Arguments
+/// * `exif` - A reference to an Exif object
 fn get_longitude(exif: &Exif) -> Option<String> {
     let longitude: Option<Vec<exif::Rational>> = exif
         .get_field(Tag::GPSLongitude, In::PRIMARY)
@@ -95,6 +131,15 @@ fn get_longitude(exif: &Exif) -> Option<String> {
     }
 }
 
+/// Converts GPS from rational to decimal format
+///
+/// This function takes in the GPS coordinates in rational format and converts it decimal format. It also takes in the hemispherical ref (N,E,S,W).
+/// It combines these two things into a string and returns it. This format is easiest to use with Google Maps.
+///
+/// # Arguments
+///
+/// * `rational` - A reference to a Vec of exif::Rational, which will have the degrees, minutes and seconds as entries in the Vec as rationals. (num/denom)
+/// * `ref_value` - A reference to a string slice containing the hemispherical value.
 fn format_gps_data(rational: &Vec<exif::Rational>, ref_value: &str) -> Option<String> {
     if rational.len() == 3 {
         let degrees = rational[0].num as f64 / rational[0].denom as f64;
@@ -114,6 +159,13 @@ fn format_gps_data(rational: &Vec<exif::Rational>, ref_value: &str) -> Option<St
     }
 }
 
+/// Processes all images in a directory, extracts metadata and sorts them chronologically
+/// 
+/// This function reads through a directory for image files. For each image in the folder it extracts the metadata, creates an ImageData struct object, and adds it to a Vec. 
+/// The objects in the Vec are stringified into JSONs and written out to a file `output.json`
+/// 
+/// # Argument
+/// * `folder_path` - a reference to a path pointing at a folder containing images
 pub fn process_images<P: AsRef<Path>>(folder_path: P) -> Result<(), String> {
     let paths: fs::ReadDir =
         fs::read_dir(folder_path).map_err(|e: std::io::Error| e.to_string())?;
@@ -141,6 +193,14 @@ pub fn process_images<P: AsRef<Path>>(folder_path: P) -> Result<(), String> {
 }
 
 // https://stackoverflow.com/questions/72392835/check-if-a-file-is-of-a-given-type
+/// Checks if the path belongs to a valid image format
+/// 
+/// This function looks at the extension of the file and examines whether or not it is of an acceptable format.
+/// Currently supports jpg, jpeg, tiff, tif and png.
+/// 
+/// # Argument
+/// 
+/// * `path` - A reference to a Path
 fn image_check(path: &Path) -> bool {
     match path.extension().and_then(std::ffi::OsStr::to_str) {
         Some(ext) => {
@@ -155,6 +215,12 @@ fn image_check(path: &Path) -> bool {
 }
 
 // https://stackoverflow.com/questions/56105305/how-to-sort-a-vec-of-structs-by-a-string-field
+/// Sorts a Vec of ImageData struct objects chronologically. 
+/// 
+/// This function takes in a Vec of ImageData objects, and sorts them by their date_created value, from oldest to newest.
+/// 
+/// # Argument
+/// * `images` - A Vec of ImageData struct objects for sorting.
 fn image_sort(mut images: Vec<ImageData>) -> Vec<ImageData> {
     images.sort_by(
         |a: &ImageData, b: &ImageData| match (&a.date_created, &b.date_created) {
